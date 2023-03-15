446e7f4baedada1ed2f1466dc72117f6d4459678
Rich Neswold <neswold@fnal.gov>
2023-03-15 07:39:41 -0500

    :sparkles: some clean-up
    - The crate converts snake_case to camelCase when creating the GraphQL
    schema so we can use snake_case in the Rust code and eliminate the
    compiler warnings.
    - The subscription now returns a stream of random readings for M:OUTTMP.

--- src/graphql/dpm/mod.rs
+++ src/graphql/dpm/mod.rs
@@ -1,27 +1,41 @@
-use std::convert::Infallible;
 use async_graphql::http::GraphiQLSource;
-use warp::{ Filter, Reply, reject, http::Response as HttpResponse };
-use futures_util::{ Stream, stream };
 use async_graphql::*;
+use async_graphql_warp::graphql_subscription;
+use futures_util::{stream, Stream};
+use std::convert::Infallible;
+use warp::{http::Response as HttpResponse, Filter, Rejection};
 
+/// The control system supports several types and this entity can repesent any of them.
 #[derive(SimpleObject)]
 struct DataType {
     value: f64,
 }
 
+/// Holds information for a data return.
 #[derive(SimpleObject)]
 struct DataInfo {
+    /// Timestamp representing when the data was sampled.
     timestamp: u64,
+
+    /// The value of the device when sampled.
     result: DataType,
+
+    /// The device's index (in the device database.)
     di: i32,
+
+    /// The name of the device.
     name: String,
+
+    /// A description of the device.
     description: String,
-    units: String,
+
+    /// The engineering units of the device's scaled value.
+    units: Option<String>,
 }
 
 #[derive(SimpleObject)]
 struct DataReply {
-    refId: i32,
+    ref_id: i32,
     cycle: u64,
     data: DataInfo,
 }
@@ -30,7 +44,7 @@ struct QueryRoot;
 
 #[Object]
 impl QueryRoot {
-    async fn acceleratorData(&self, drfs: Vec<String>) -> Vec<DataReply> {
+    async fn accelerator_data(&self, drfs: Vec<String>) -> Vec<DataReply> {
         vec![]
     }
 }
@@ -39,28 +53,72 @@ struct SubscriptionRoot;
 
 #[Subscription]
 impl SubscriptionRoot {
-    async fn acceleratorData(&self, drfs: Vec<String>) -> impl Stream<Item = DataReply> {
-        stream::empty()
+    async fn accelerator_data(
+        &self, drfs: Vec<String>,
+    ) -> impl Stream<Item = DataReply> {
+        use rand::{rngs::SmallRng, Rng, SeedableRng};
+
+        stream::unfold(
+            (SmallRng::from_entropy(), 70.0),
+            |(mut rng, value)| async move {
+                tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+
+                let noise = rng.gen_range(0.0..0.3) - 0.15;
+
+                Some((
+                    DataReply {
+                        ref_id: 0,
+                        cycle: 1,
+                        data: DataInfo {
+                            timestamp: 100,
+                            result: DataType { value },
+                            di: 1000,
+                            name: "M:OUTTMP".to_string(),
+                            description: "Outdoor temperature".to_string(),
+                            units: Some("TEMP".to_string()),
+                        },
+                    },
+                    (rng, value + noise),
+                ))
+            },
+        )
     }
 }
 
 type MySchema = Schema<QueryRoot, EmptyMutation, SubscriptionRoot>;
 
-pub async fn filter() {
-    let schema = Schema::build(QueryRoot, EmptyMutation, SubscriptionRoot)
-        .finish();
-    let filter = async_graphql_warp::graphql(schema)
-        .and_then(|(schema, request): (MySchema, async_graphql::Request)| async move {
-            let resp = schema.execute(request).await;
+pub fn filter(
+) -> impl Filter<Extract = (impl warp::Reply,), Error = Rejection> + Clone {
+    let schema =
+        Schema::build(QueryRoot, EmptyMutation, SubscriptionRoot).finish();
 
-            Ok::<_, Infallible>(async_graphql_warp::GraphQLResponse::from(resp))
-        }
-    );
+    let graphql_query = warp::path("q")
+        .and(warp::path::end())
+        .and(warp::post())
+        .and(async_graphql_warp::graphql(schema.clone()).and_then(
+            |(schema, request): (MySchema, async_graphql::Request)| async move {
+                let resp = schema.execute(request).await;
+
+                Ok::<_, Infallible>(async_graphql_warp::GraphQLResponse::from(
+                    resp,
+                ))
+            },
+        ));
+
+    let graphql_sub = warp::path("s")
+        .and(warp::path::end())
+        .and(graphql_subscription(schema));
+
     let graphiql = warp::path::end().and(warp::get()).map(|| {
         HttpResponse::builder()
             .header("content-type", "text/html")
-            .body(GraphiQLSource::build().endpoint("/").finish())
+            .body(
+                GraphiQLSource::build()
+                    .endpoint("/dpm/q")
+                    .subscription_endpoint("/dpm/s")
+                    .finish(),
+            )
     });
-    
-    warp::serve(graphiql.or(filter)).run(([127, 0, 0, 1], 8000)).await
+
+    warp::path("dpm").and(graphiql.or(graphql_query).or(graphql_sub))
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod graphql;
 
 #[tokio::main]
 async fn main() {
-    graphql::dpm::filter().await
-
-    //warp::serve(graphql::dpm::filter()).run(([127, 0, 0, 1], 8000)).await;
+    warp::serve(graphql::dpm::filter())
+        .run(([127, 0, 0, 1], 8000))
+        .await;
 }
