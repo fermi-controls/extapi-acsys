991e1b92e66e87b356567b813e80ce6ec4df7823
Rich Neswold <neswold@fnal.gov>
2023-05-22 13:41:47 -0500

    Add support for the gRPC-based, devdb service.

--- src/g_rpc/devdb/mod.rs
+++ src/g_rpc/devdb/mod.rs
@@ -1,4 +1,19 @@
+use proto::dev_db_client::DevDbClient;
 
 pub mod proto {
     tonic::include_proto!("devdb");
+}
+
+pub async fn get_device_info(
+    device: String,
+) -> Result<tonic::Response<proto::DeviceInfoReply>, tonic::Status> {
+    let mut client = DevDbClient::connect("http://clx76.fnal.gov:6802/")
+        .await
+        .unwrap();
+
+    let req = proto::DeviceList {
+        device: vec![device],
+    };
+
+    client.get_device_info(req).await
 }
--- src/graphql/acsys/handlers/mod.rs
+++ src/graphql/acsys/handlers/mod.rs
@@ -2,6 +2,7 @@ use crate::g_rpc::dpm::proto;
 use async_graphql::*;
 use futures_util::{Stream, StreamExt};
 use tonic::Status;
+use tracing::{error, info, warn};
 
 mod types;
 
@@ -13,6 +14,43 @@ impl QueryRoot {
         &self, _drfs: Vec<String>,
     ) -> Vec<types::DataReply> {
         vec![]
+    }
+
+    async fn device_info(&self, device: String) -> types::DeviceInfo {
+        use crate::g_rpc::devdb::{self, proto};
+
+        info!("looking up {}", &device);
+
+        match devdb::get_device_info(device).await {
+            Ok(s) => {
+                if let Some(proto::info_entry::Result::Device(di)) =
+                    &s.into_inner().set[0].result
+                {
+                    types::DeviceInfo {
+                        description: di.description.clone(),
+                        reading: di.reading.as_ref().map(|p| {
+                            types::DeviceProperty {
+                                primary_units: p.primary_units.clone(),
+                                common_units: p.common_units.clone(),
+                            }
+                        }),
+                        setting: di.setting.as_ref().map(|p| {
+                            types::DeviceProperty {
+                                primary_units: p.primary_units.clone(),
+                                common_units: p.common_units.clone(),
+                            }
+                        }),
+                    }
+                } else {
+                    error!("postgres error");
+                    todo!()
+                }
+            }
+            Err(e) => {
+                error!("gRPC error: {:?}", &e);
+                todo!()
+            }
+        }
     }
 }
 
